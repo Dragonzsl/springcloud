f47eba9f121550b596229b13333cf11ce17176b7
zsl <1335026358@qq.com>
2020-09-30 19:01:07 +0800

    spring cloud demo

---
+++ cloud-config-client-3355/src/main/java/com/shilin/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,17 @@
+package com.shilin.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author shilin
+ * @create 2020-09-30 13:10
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
